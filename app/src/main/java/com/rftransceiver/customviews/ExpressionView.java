d7349188b394622d8b8f1c48b514151d6f410a09
YancyWu <405586451@qq.com>
2015-07-26 21:14:15 +0800

    界面优化
    底部功能视图优化

---
+++ app/src/main/java/com/rftransceiver/customviews/ExpressionView.java
@@ -0,0 +1,23 @@
+package com.rftransceiver.customviews;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.GridView;
+
+/**
+ * Created by Wuyang on 2015/7/26.
+ */
+public class ExpressionView extends GridView{
+    public ExpressionView(Context context) {
+        super(context);
+    }
+
+    public ExpressionView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public ExpressionView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+}
